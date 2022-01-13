787bd92d84c0d3bb1449007801cf8d90fd001290
Dimitriy1999 <97621373+Dimitriy1999@users.noreply.github.com>
2022-01-13 13:07:59 -0800

    Formatting everything

--- Unit-02/src/Cube Runner.java
+++ Unit-02/src/Cube Runner.java
@@ -3,17 +3,15 @@
 //Name Dimitriy Cheverdyuk
 //Date 1/12/2022
 
-public class CubeRunner
-{
-	public static void main( String[] args )
-   {
-    Cube cube = new Cube();
-		System.out.println( "Cube area is :: " + cube.area( 112 ) );
-		System.out.println( "Cube area is :: " + cube.area( 4 ) );
-		System.out.println( "Cube area is :: " + cube.area( 33 ) );
-		System.out.println( "Cube area is :: " + cube.area( 50 ) );
-		System.out.println( "Cube area is :: " + cube.area( 5 ) );		
-		System.out.println( "Cube area is :: " + cube.area( 19 ) );
-		System.out.println( "Cube area is :: " + cube.area( 111 ) );
-	}
+public class CubeRunner {
+    public static void main(String[] args) {
+        Cube cube = new Cube();
+        System.out.println("Cube area is :: " + cube.area(112));
+        System.out.println("Cube area is :: " + cube.area(4));
+        System.out.println("Cube area is :: " + cube.area(33));
+        System.out.println("Cube area is :: " + cube.area(50));
+        System.out.println("Cube area is :: " + cube.area(5));
+        System.out.println("Cube area is :: " + cube.area(19));
+        System.out.println("Cube area is :: " + cube.area(111));
+    }
 }
