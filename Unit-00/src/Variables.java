133b528430c55b7f43fa16e13511f05e9aec29bc
Dimitriy1999 <97621373+Dimitriy1999@users.noreply.github.com>
2022-01-13 13:06:31 -0800

    Formatting so it's easier to read

--- Unit-00/src/Variables.java
+++ Unit-00/src/Variables.java
@@ -1,56 +1,54 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
+  //(c) A+ Computer Science
+  //www.apluscompsci.com
 
-//Name Dimitriy
-//Date 1/12/2022
-//Class AP Computer Science A
-//Lab  Unit 0
+  //Name Dimitriy
+  //Date 1/12/2022
+  //Class AP Computer Science A
+  //Lab  Unit 0
 
-public class Variables
-{
-	public static void main ( String[] args )
-	{
-     //define 1 variable of each of the
-		//following data types
-		//byte		
-    		//short		
-    		//int  		
-    		//long
-		//float		
-    		//double
-		//char      
-    		//boolean		
-    		//String
+  public class Variables {
+      public static void main(String[] args) {
+          //define 1 variable of each of the
+          //following data types
+          //byte		
+          //short		
+          //int  		
+          //long
+          //float		
+          //double
+          //char      
+          //boolean		
+          //String
 
-		//integer variables
-		byte byteOne = 127;
-    		int randomNumber = 100;
+          //integer variables
+          byte byteOne = 127;
+          int randomNumber = 100;
 
-		//decimal variables
-    		float number = 19.5f;
-    		double number2 = 15.42;
+          //decimal variables
+          float number = 19.5 f;
+          double number2 = 15.42;
 
-		//other integer types
-    		long bigNumber = 1231231233;
-    		short shortNumber = 32767;
-		//other types
-    		String text = "random Text";
-   		 char oneLetter = 't';
-    		boolean isHuman = true;
+          //other integer types
+          long bigNumber = 1231231233;
+          short shortNumber = 32767;
+          //other types
+          String text = "random Text";
+          char oneLetter = 't';
+          boolean isHuman = true;
 
-		//output your information here
-	System.out.println("/////////////////////////////////");
-	System.out.println("*                               *");
-	System.out.println("*        integer types          *");
-	System.out.println("*                               *");
-	System.out.println("*8 bit - byteOne = "+byteOne+"\t\t    *");
-	System.out.println("*int Number = "+randomNumber+"\t\t         *");
-	System.out.println("*float float number = "+number+"\t\t       *");
-	System.out.println("*double number  = "+number2+"\t\t        *");
-	System.out.println("*long big Number  = "+bigNumber+"\t\t    *");
-	System.out.println("*short number  = "+shortNumber+"\t\t*");
-	System.out.println("*string text  = "+text+"\t\t   *");
-	System.out.println("*char one Letter / Digit  = "+oneLetter+"\t\t     *");
-	System.out.println("*boolean isHuman  = "+isHuman+"\t\t    *");	
-    }
-}
+          //output your information here
+          System.out.println("/////////////////////////////////");
+          System.out.println("*                               *");
+          System.out.println("*        integer types          *");
+          System.out.println("*                               *");
+          System.out.println("*8 bit - byteOne = " + byteOne + "\t\t    *");
+          System.out.println("*int Number = " + randomNumber + "\t\t         *");
+          System.out.println("*float float number = " + number + "\t\t       *");
+          System.out.println("*double number  = " + number2 + "\t\t        *");
+          System.out.println("*long big Number  = " + bigNumber + "\t\t    *");
+          System.out.println("*short number  = " + shortNumber + "\t\t*");
+          System.out.println("*string text  = " + text + "\t\t   *");
+          System.out.println("*char one Letter / Digit  = " + oneLetter + "\t\t     *");
+          System.out.println("*boolean isHuman  = " + isHuman + "\t\t    *");
+      }
+  }
