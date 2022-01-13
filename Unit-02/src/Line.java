83ec88fde80da969f21caabca9b5f4ed4002c3a2
Dimitriy1999 <97621373+Dimitriy1999@users.noreply.github.com>
2022-01-13 13:09:36 -0800

    Formatting everything

--- Unit-02/src/Line.java
+++ Unit-02/src/Line.java
@@ -3,10 +3,9 @@
 //Name Dimitriy Cheverdyuk
 //Date 1/12/2022
 
-public class Line
-{
-	public static double getSlope( int x1, int y1, int x2, int y2 )
-	{
-		return (y2 - y1) / (x2 - x1);
-	}
+public class Line {
+    public static double getSlope(int x1, int y1, int x2, int y2) 
+    {
+        return (y2 - y1) / (x2 - x1);
+    }
 }
