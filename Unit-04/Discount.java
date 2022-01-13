56346674e6381322d66cd3d1f32230315530ead8
Dimitriy1999 <97621373+Dimitriy1999@users.noreply.github.com>
2022-01-13 14:05:15 -0800

    Finished Discount

---
+++ Unit-04/Discount.java
@@ -0,0 +1,14 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+public class Discount {
+    public static double getDiscountedBill(double bill) {
+        double finalValue = bill;
+        double discountAmount = 15;
+        if (bill > 2000) {
+            discountAmount = discountAmount / 100;
+            discountAmount = bill * discountAmount;
+            finalValue = bill - discountAmount;
+        }
+        return finalValue;
+    }
+}
