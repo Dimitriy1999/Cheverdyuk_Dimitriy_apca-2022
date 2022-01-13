7d466bcf391612956a20a49acf92cbcae0f26811
Dimitriy1999 <97621373+Dimitriy1999@users.noreply.github.com>
2022-01-13 13:07:12 -0800

    Formatting everything

--- Unit-01/src/Stars Runner.java
+++ Unit-01/src/Stars Runner.java
@@ -8,19 +8,17 @@
 
 import static java.lang.System.*;
 
-public class StarsRunner
-{
-   public static void main(String args[])
-   {
-       StarsAndStripes flag = new StarsAndStripes();
-      flag.printTwentyStars();
-      flag.printTwentyStars();
-      flag.printTwentyDashes();
-      flag.printTwentyStars();
-      flag.printTwentyStars();
-      flag.printTwoBlankLines();
-      flag.printTwentyDashes();
-      flag.printTwentyDashes();
-      
-   }
+public class StarsRunner {
+    public static void main(String args[]) {
+        StarsAndStripes flag = new StarsAndStripes();
+        flag.printTwentyStars();
+        flag.printTwentyStars();
+        flag.printTwentyDashes();
+        flag.printTwentyStars();
+        flag.printTwentyStars();
+        flag.printTwoBlankLines();
+        flag.printTwentyDashes();
+        flag.printTwentyDashes();
+
+    }
 }
