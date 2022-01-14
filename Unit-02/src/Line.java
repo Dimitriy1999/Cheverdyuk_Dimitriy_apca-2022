378ae2b742d35e104d353b1811453de9bf3308fc
Dimitriy1999 <97621373+Dimitriy1999@users.noreply.github.com>
2022-01-14 09:01:03 -0800

    Added Double cast as it would not return me decimal values and only whole numbers

--- Unit-02/src/Line.java
+++ Unit-02/src/Line.java
@@ -6,6 +6,7 @@
 public class Line {
     public static double getSlope(int x1, int y1, int x2, int y2) 
     {
-        return (y2 - y1) / (x2 - x1);
+    	double result = (double)(y2 - y1) / (x2 - x1);
+        return result;
     }
 }
