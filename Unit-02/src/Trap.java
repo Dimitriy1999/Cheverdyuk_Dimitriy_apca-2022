d8aafad1da22f1c287595745235911dd77445390
Dimitriy1999 <97621373+Dimitriy1999@users.noreply.github.com>
2022-01-14 09:04:40 -0800

    Added Cast because it would not return decimal value + fixed the code a bit

--- Unit-02/src/Trap.java
+++ Unit-02/src/Trap.java
@@ -8,6 +8,6 @@ public class Trap
 {
 	public static double area( int base1, int base2, int height )
 	{
-		return 1/2 * (base1 + base2) * height;
+		return (double)((base1 + base2) * height) * 1/2;
 	}
 }
