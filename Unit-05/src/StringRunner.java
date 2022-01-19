c7b30ef59b5a64804a515da214bf8e20ffd0d260
Dimitriy1999 <97621373+Dimitriy1999@users.noreply.github.com>
2022-01-19 10:34:07 -0800

    Formatted Again

--- Unit-05/src/StringRunner.java
+++ Unit-05/src/StringRunner.java
@@ -6,35 +6,33 @@
 //Class -
 //Lab  -
 
-import static java.lang.System.*;
 import java.util.Scanner;
 
-public class StringRunner
-{
-	public static void main ( String[] args )
+public class StringRunner {
+	public static void main(String[] args) 
 	{
 		Scanner keyboard = new Scanner(System.in);
 		StringOddOrEven test = new StringOddOrEven();
-		
-			System.out.print("Enter an word :: ");
-			String word = keyboard.next();
-			test.setString(word);
-			System.out.println(test.toString());
-        
-			System.out.print("Enter an word :: ");
-			word = keyboard.next();
-			test.setString(word);
-        	System.out.println(test.toString());
 
-        	System.out.print("Enter an word :: ");
-        	word = keyboard.next();
-        	test.setString(word);
-        	System.out.println(test.toString());
+		System.out.print("Enter an word :: ");
+		String word = keyboard.next();
+		test.setString(word);
+		System.out.println(test.toString());
 
-        	System.out.print("Enter an word :: ");
-        	word = keyboard.next();
-        	test.setString(word);
-        	System.out.println(test.toString());
+		System.out.print("Enter an word :: ");
+		word = keyboard.next();
+		test.setString(word);
+		System.out.println(test.toString());
+
+		System.out.print("Enter an word :: ");
+		word = keyboard.next();
+		test.setString(word);
+		System.out.println(test.toString());
+
+		System.out.print("Enter an word :: ");
+		word = keyboard.next();
+		test.setString(word);
+		System.out.println(test.toString());
 
 	}
 }
