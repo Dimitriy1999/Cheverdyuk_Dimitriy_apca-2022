674b349f22c900347ad3cb770a8b6711307d32d8
Dimitriy1999 <97621373+Dimitriy1999@users.noreply.github.com>
2022-01-24 09:29:10 -0800

    Finished Word and Word Runner so I am done with Unit 6
    Finally Added a comment

--- Unit-06/src/Word.java
+++ Unit-06/src/Word.java
@@ -10,34 +10,43 @@ public class Word
 
 	public Word()
 	{
+		word = "test";
 	}
 
 	public Word(String s)
 	{
+		word = s;
 	}
 
 	public void setString(String s)
 	{
+		word = s;
 	}
 
 	public char getFirstChar()
 	{
-		return 0;
+		return word.charAt(0);
 	}
 
 	public char getLastChar()
 	{
-		return 0;
+		return word.charAt(word.length() - 1);
 	}
 
 	public String getBackWards()
 	{
 		String back="";
+		//Going backwards in the array so we start from the back instead of the front then just add that to a string
+		char [] arr = word.toCharArray();
+		for(int i = arr.length - 1; i >= 0; i--)
+		{
+			back = back + arr[i];
+		}
 		return back;
 	}
 
  	public String toString()
  	{
- 		return "";
+ 		return getFirstChar() + "\n" + getLastChar() + "\n" + getBackWards() + "\n" + word + "\n";
 	}
 }
--- Unit-06/src/WordRunner.java
+++ Unit-06/src/WordRunner.java
@@ -8,6 +8,16 @@ public class WordRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		Word test = new Word();
+		test.setString("Hello");
+		System.out.println(test.toString());
+		test.setString("World");
+		System.out.println(test.toString());
+		test.setString("JukeBox");
+		System.out.println(test.toString());
+		test.setString("TCEA");
+		System.out.println(test.toString());
+		test.setString("UIL");
+		System.out.println(test.toString());
 	}
 }
