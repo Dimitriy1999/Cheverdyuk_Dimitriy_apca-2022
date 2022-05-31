c9c222e49c225075c1a1432f25cba08aa039deb0
Dimitriy1999 <97621373+Dimitriy1999@users.noreply.github.com>
2022-05-31 15:56:34 -0700

    Finished Perceptron

--- Neural Network/src/Perception.java
+++ Neural Network/src/Perception.java
@@ -1,23 +1,23 @@
 
-public class Perception 
+public class Perceptron 
 {
 	private int threshold;
-	private int learningRate;
-	private int bias;
-	private int [] weight;
-	public Perception(int threshold, int learningRate, int [] arr)
+	private double learningRate;
+	private double bias;
+	private double [] weight;
+	public Perceptron(int numberOfInputs, int threshold, double learningRate)
 	{
 		this.threshold = threshold;
 		this.learningRate = learningRate;
-		bias = 0;
-		weight = new int[5];
+		this.bias = 0;
+		weight = new double[numberOfInputs];
 	}
 	
-	public int Predict(int [] inputs)
+	public int predict(double [] inputs)
 	{
 		int activation = 0;
-		int z = dot(inputs, weight) + bias;
-		System.out.println(inputs[0] + " * " + weight[0] + " + " + inputs[1] + " * " + weight[1] + " = " + z);
+		double z = dot(inputs, weight) + bias;
+		System.out.println("z = " + inputs[0] + " * " + weight[0] + " + " + inputs[1] + " * " + weight[1] + " = " + z);
 		if(z > 0)
 		{
 			activation = 1;
@@ -29,32 +29,76 @@ public class Perception
 		return activation;
 	}
 	
-	public int dot(int [] input, int[] weight)
-	{
-		int result = 0;
-		for(int i = 0; i < input.length; i++)
-		{
-			result = input[i] * weight[i];
-		}
-		return result;
+	public double dot(double[] q, double[]p){
+
+	    double scalarproduct = 0.0;
+
+	    if (q.length == p.length){
+
+	        for(int i = 0; i < q.length; i++){
+	            scalarproduct = scalarproduct + q[i] * p[i];
+	        }
+	    }
+	    return scalarproduct;
 	}
 	
-	public void Train(int [][] trainingInputs, int epoch)
+	public void train(double [][] trainingInputs, double [] labels)
 	{
 		for(int i = 0; i < threshold; i++)
 		{
-			for(int j = 0; j < trainingInputs.length; j++)
+			System.out.println("---------" + "Number of Iterations " +  i + "----------");
+			for(int j = 0; j < labels.length; j++)
 			{
-				for(int k = 0; k < trainingInputs[0].length; k++)
+				double [] inputs = trainingInputs[j];
+				double label = labels[j];
+				System.out.println("Current Inputs " + arrayToString(inputs) + " " + " Weight " + arrayToString(weight) + 
+				" Bias " + bias);
+				double yhat = predict(inputs);
+				System.out.println("Yhat: "+ yhat);
+				double error = label - yhat;
+				System.out.println("Error = " + label + " - " + yhat + " = "+ error);
+				for(int k = 0; k < inputs.length; k++)
 				{
-					
+					weight[k] += learningRate * error * inputs[k];
 				}
+				bias += learningRate * error;
+				System.out.println("Updated Weight " + arrayToString(weight));
+				System.out.println("Updated Bias " + bias + "\n");
 			}
 		}
 	}
 	
+	public static String arrayToString(double [] arr)
+	{
+		String result = "[";
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(i > 0)
+			{
+				result = result + ", ";
+			}
+			result += arr[i];
+		}
+		return result + "]";
+	}
+	
 	public static void main(String[] args)
 	{
-		
+		double [] labels = new double[]{1,0,0,0};
+		double [][] matrix = new double[][] 
+				{
+					new double[] {1,1},
+					new double[] {1,0},
+					new double[] {0,1},
+					new double[] {0,0}
+				};
+		Perceptron perceptron = new Perceptron(2, 10, 1);
+		perceptron.train(matrix, labels);
+		int a = 1;
+		int b = 0;
+		double [] inputs = new double[] {a,b};
+		System.out.println("---------" + "Input" + "----------" + arrayToString(inputs));
+		int output = perceptron.predict(inputs);
+		System.out.println("---------" + "outut" + "----------" + " " + output);
 	}
 }
